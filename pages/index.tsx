b80eceb61ac955aa2efeef4519ab205fecd3d646
Craig Wheeler <craig@craigwheeler.dev>
2022-10-21 15:58:42 -0400

    Add switches component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import { Button, Group } from "@mantine/core";
+import { Group } from "@mantine/core";
+import SwitchesCard from "../components/SwitchesCard";
 
 export default function IndexPage() {
   return (
     <Group mt={50} position="center">
-      <Button size="xl">Welcome to Mantine!</Button>
+      <SwitchesCard
+        title="My IoT devices"
+        description="Toggle devices on and off"
+        data={[
+          {
+            id: 0,
+            title: "Switch 1",
+            description: "Description for switch 1",
+          },
+          {
+            id: 1,
+            title: "Switch 2",
+            description: "Description for switch 1",
+          },
+          {
+            id: 2,
+            title: "Switch 3",
+            description: "Description for switch 1",
+          },
+        ]}
+      />
     </Group>
   );
 }
