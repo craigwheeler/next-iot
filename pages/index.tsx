d0d6c9aed4ad7411f45ad900624ad4af4ecca84a
Craig Wheeler <craig@craigwheeler.dev>
2022-10-21 16:51:54 -0400

    Add header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,56 @@
-import { Group } from "@mantine/core";
+import { Group, NavLink } from "@mantine/core";
 import SwitchesCard from "../components/SwitchesCard";
 import StatsRingCard from "../components/StatsRingCard";
+import HeaderMiddle from "../components/HeaderMiddle";
 
 export default function IndexPage() {
   return (
-    <Group mt={50} position="center">
-      <StatsRingCard
-        title="Power Consumption"
-        completed={7}
-        total={20}
-        stats={[
-          { label: "Current", value: 18 },
-          { label: "In Progress", value: 8 },
+    <>
+      <HeaderMiddle
+        links={[
+          {
+            link: "/about",
+            label: "Home",
+          },
+          {
+            link: "/learn",
+            label: "Features",
+          },
+          {
+            link: "/contact",
+            label: "Contact",
+          },
         ]}
       />
 
-      <SwitchesCard
-        title="My Switches"
-        description="Toggle devices on and off"
-        data={[
-          {
-            id: 0,
-            title: "Switch 1",
-            description: "Description for switch 1",
-          },
-          {
-            id: 1,
-            title: "Switch 2",
-            description: "Description for switch 1",
-          },
-        ]}
-      />
-    </Group>
+      <Group mt={50} position="center">
+        <StatsRingCard
+          title="Power Consumption"
+          completed={7}
+          total={20}
+          stats={[
+            { label: "Current", value: 18 },
+            { label: "In Progress", value: 8 },
+          ]}
+        />
+
+        <SwitchesCard
+          title="My Switches"
+          description="Toggle devices on and off"
+          data={[
+            {
+              id: 0,
+              title: "Switch 1",
+              description: "Description for switch 1",
+            },
+            {
+              id: 1,
+              title: "Switch 2",
+              description: "Description for switch 1",
+            },
+          ]}
+        />
+      </Group>
+    </>
   );
 }
