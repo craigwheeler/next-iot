4d5f59a035cfd122c1492104b0cb9ae18fcd42b4
Craig Wheeler <craig@craigwheeler.dev>
2022-10-21 16:25:00 -0400

    Add stats ring card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import { Group } from "@mantine/core";
 import SwitchesCard from "../components/SwitchesCard";
+import StatsRingCard from "../components/StatsRingCard";
 
 export default function IndexPage() {
   return (
     <Group mt={50} position="center">
+      <StatsRingCard
+        title="Power Consumption"
+        completed={7}
+        total={20}
+        stats={[
+          { label: "Current", value: 18 },
+          { label: "In Progress", value: 8 },
+        ]}
+      />
+
       <SwitchesCard
-        title="My IoT devices"
+        title="My Switches"
         description="Toggle devices on and off"
         data={[
           {
@@ -16,11 +27,6 @@ export default function IndexPage() {
           {
             id: 1,
             title: "Switch 2",
-            description: "Description for switch 1",
-          },
-          {
-            id: 2,
-            title: "Switch 3",
             description: "Description for switch 1",
           },
         ]}
